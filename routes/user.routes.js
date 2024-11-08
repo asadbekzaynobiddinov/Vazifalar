e456cd6f00b498cdd444737f518df605780c8c6c
Asadbek Zaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2024-11-08 09:37:30 +0500

    controller funkisya qolib ketgan ekan :)

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,7 @@ import { checkRole } from "../middleware/index.js"
 
 export const usersRouter = Router()
 
-usersRouter.get('/me/:id', checkRole(['SuperAdmin', 'Admin', 'User']))
+usersRouter.get('/me/:id', checkRole(['SuperAdmin', 'Admin', 'User']), getMyAccountInfoController)
 usersRouter.post('/admin', checkRole(['SuperAdmin', 'Admin']), registerAdminController)
 usersRouter.get('/all', checkRole(['SuperAdmin', 'Admin']), getAllUsersController)
 usersRouter.put('/update/:id', checkRole(['SuperAdmin', 'Admin']), updatedUserController)
