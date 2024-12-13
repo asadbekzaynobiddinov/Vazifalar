60a2e519e1a4f79cff0c39453a187389a820336a
Asadbek Zaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2024-12-13 16:54:30 +0500

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+    MongooseModule.forRoot('mongodb://localhost/21017/blog'),
+  ],
+})
+export class AppModule {}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ required: true })
+  first_name: string;
+
+  @Prop({ required: true })
+  last_name: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
